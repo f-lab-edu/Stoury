a8bfe0f582d8fa3a957feeace0c0e9a5eaa81225
Jzakka <mouse4786@gmail.com>
2024-01-10 23:11:05 +0900

    setup

---
+++ src/main/java/com/stoury/StouryApplication.java
@@ -0,0 +1,13 @@
+package com.stoury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StouryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StouryApplication.class, args);
+    }
+
+}
