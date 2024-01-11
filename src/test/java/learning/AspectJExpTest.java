e5a11704980b9ab337599edd8c7ce82831af8ed8
Jzakka <mouse4786@gmail.com>
2024-01-11 20:30:40 +0900

    Test: aspectJ 표현식 테스트

---
+++ src/test/java/learning/AspectJExpTest.java
@@ -0,0 +1,23 @@
+package learning;
+
+import com.stoury.dto.MemberDto;
+import com.stoury.service.BasicMemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+public class AspectJExpTest {
+    @Test
+    @DisplayName("execution(* com.stoury.service.*Service.*(..))이 BasicMemberService의 메소드에 부합해야함")
+    void expressionTest() throws NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.stoury.service.*Service.*(..))");
+
+        Method createMember = BasicMemberService.class.getMethod("createMember", MemberDto.class);
+
+        Assertions.assertThat(pointcut.matches(createMember, BasicMemberService.class)).isTrue();
+    }
+}
