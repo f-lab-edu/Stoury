2f7e9859df2f66312dbb9786053d933e189f881b
Jzakka <mouse4786@gmail.com>
2024-01-13 00:43:31 +0900

    Feat: 사용자 검색 페이지네이션 + 검색어 공백 삭제

--- src/main/java/com/stoury/repository/MemberRepository.java
+++ src/main/java/com/stoury/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.stoury.repository;
 
 import com.stoury.domain.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findAllByDeleted(boolean deleted);
 
-    List<Member> findAllByUsernameLikeIgnoreCaseOrderByUsername(String username);
+    List<Member> findAllByUsernameLikeIgnoreCase(String username, Pageable page);
 }
