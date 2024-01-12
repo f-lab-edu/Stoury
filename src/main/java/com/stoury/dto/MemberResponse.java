a7b9c0fce1e8f8f3c6fb1788b282328c12d065c0
Jzakka <mouse4786@gmail.com>
2024-01-12 23:47:44 +0900

    사용자 소개글도 응답값에 포함

--- src/main/java/com/stoury/dto/MemberResponse.java
+++ src/main/java/com/stoury/dto/MemberResponse.java
@@ -4,11 +4,12 @@ import com.stoury.domain.Member;
 import lombok.Builder;
 
 @Builder
-public record MemberResponse(String email, String username, String profileImagePath) {
+public record MemberResponse(String email, String username, String profileImagePath, String introduction) {
     public static MemberResponse from(Member member) {
         return new MemberResponse(
                 member.getEmail(),
                 member.getUsername(),
-                member.getProfileImagePath());
+                member.getProfileImagePath(),
+                member.getIntroduction());
     }
 }
