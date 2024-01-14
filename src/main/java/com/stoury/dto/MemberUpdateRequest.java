3c4219fab45dbe510b7a5bd70209267e5aae8a9e
Jzakka <mouse4786@gmail.com>
2024-01-14 12:44:22 +0900

    MemberUpdateRequest 가 id 정보를 포함

---
+++ src/main/java/com/stoury/dto/MemberUpdateRequest.java
@@ -0,0 +1,7 @@
+package com.stoury.dto;
+
+import lombok.Builder;
+
+@Builder
+public record MemberUpdateRequest(Long id, String email, String username, String profileImagePath, String introduction) {
+}
