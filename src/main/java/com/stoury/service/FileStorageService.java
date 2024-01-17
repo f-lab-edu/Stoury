4b8d76466c56f6e5a6d02c40bcab06a63544e587
Jzakka <mouse4786@gmail.com>
2024-01-17 17:10:46 +0900

    Feat: 지정 경로에 파일 저장

---
+++ src/main/java/com/stoury/service/FileStorageService.java
@@ -0,0 +1,32 @@
+package com.stoury.service;
+
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+@Component
+public class FileStorageService implements StorageService {
+    @Override
+    public void saveFilesAtPath(Pair<MultipartFile, String> reservedImagePaths) {
+        MultipartFile fileToSave = reservedImagePaths.getFirst();
+        String fullPath = reservedImagePaths.getSecond();
+        int lastIndexOfSlash = fullPath.lastIndexOf("/");
+
+        String directory = fullPath.substring(0, lastIndexOfSlash);
+        String fileName = fullPath.substring(lastIndexOfSlash + 1);
+
+        Path path = Paths.get(directory, fileName);
+
+        try {
+            if (Files.exists(path)) {
+                throw new FileAlreadyExistsException(fullPath);
+            }
+            Files.copy(fileToSave.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/stoury/service/StorageService.java
+++ src/main/java/com/stoury/service/StorageService.java
@@ -6,5 +6,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface StorageService {
+    /**
+     * reservedImagePaths.first: 저장해야할 파일
+     * reservedImagePaths.second: 저장해야할 경로
+     * @param reservedImagePaths
+     */
     void saveFilesAtPath(Pair<MultipartFile, String> reservedImagePaths);
 }
