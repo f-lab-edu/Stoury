df04d374789d0bf43ddb4b9e8ca4a00150e80976
Jzakka <mouse4786@gmail.com>
2024-01-23 16:54:13 +0900

    Test: 사용자 실패 테스트
    - 존재하지 않는 사용자
    - 존재하지 않는 피드

--- src/test/java/com/stoury/service/LikeServiceTest.java
+++ src/test/java/com/stoury/service/LikeServiceTest.java
@@ -3,15 +3,17 @@ package com.stoury.service;
 import com.stoury.domain.Feed;
 import com.stoury.domain.Like;
 import com.stoury.domain.Member;
+import com.stoury.exception.FeedSearchException;
+import com.stoury.exception.MemberCrudExceptions;
 import com.stoury.repository.FeedRepository;
 import com.stoury.repository.LikeRepository;
 import com.stoury.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -41,5 +43,30 @@ public class LikeServiceTest {
         likeService.like(dummyLiker, dummyFeed);
 
         verify(likeRepository, times(1)).save(any(Like.class));
+    }
+
+    @Test
+    @DisplayName("좋아요 실패 - 존재하지 않는 사용자")
+    void likeFailByNotExistingMember() {
+        when(memberRepository.existsById(any())).thenReturn(false);
+
+        Member dummyLiker = Member.builder().build();
+        Feed dummyFeed = Feed.builder().build();
+
+        Assertions.assertThatThrownBy(()->likeService.like(dummyLiker, dummyFeed))
+                .isInstanceOf(MemberCrudExceptions.MemberSearchException.class);
+    }
+
+    @Test
+    @DisplayName("좋아요 실패 - 존재하지 않는 피드")
+    void likeFailByNotExistingFeed() {
+        when(memberRepository.existsById(any())).thenReturn(true);
+        when(feedRepository.existsById(any())).thenReturn(false);
+
+        Member dummyLiker = Member.builder().build();
+        Feed dummyFeed = Feed.builder().build();
+
+        Assertions.assertThatThrownBy(()->likeService.like(dummyLiker, dummyFeed))
+                .isInstanceOf(FeedSearchException.class);
     }
 }
