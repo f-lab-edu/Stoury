6958ec40cbb00ac7f7b4068d34abb349cab16a80
Jzakka <mouse4786@gmail.com>
2024-01-26 16:36:19 +0900

    Feat: 대댓글 기능 - 대댓글은 한 단계밖에 못함. 대대댓글 같은 거 없음.

--- src/main/java/com/stoury/exception/CommentSearchException.java
+++ src/main/java/com/stoury/exception/CommentSearchException.java
@@ -1,10 +1,6 @@
 package com.stoury.exception;
 
 public class CommentSearchException extends RuntimeException {
-    public CommentSearchException() {
-        this("Cannot find comment");
-    }
-
     public CommentSearchException(String message) {
         super(message);
     }
