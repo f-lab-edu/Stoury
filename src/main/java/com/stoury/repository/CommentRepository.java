4bb5eb381c2d33aa59c01cf88e73c54347bf2c0c
Jzakka <mouse4786@gmail.com>
2024-01-26 17:12:26 +0900

    Feat: 피드의 댓글 가져오기

--- src/main/java/com/stoury/repository/CommentRepository.java
+++ src/main/java/com/stoury/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.stoury.repository;
 
 import com.stoury.domain.Comment;
+import com.stoury.domain.Feed;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByFeedAndCreatedAtBefore(Feed feed, LocalDateTime orderThan, Pageable pageable);
 }
