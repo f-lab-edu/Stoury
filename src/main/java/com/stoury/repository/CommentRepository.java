6db60d6a2b901f5c2fc0ce88a9617cdfad0d8339
Jzakka <mouse4786@gmail.com>
2024-01-26 17:33:25 +0900

    Feat: 대댓글 조회

--- src/main/java/com/stoury/repository/CommentRepository.java
+++ src/main/java/com/stoury/repository/CommentRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByFeedAndCreatedAtBefore(Feed feed, LocalDateTime orderThan, Pageable pageable);
+
+    List<Comment> findAllByParentCommentAndCreatedAtBefore(Comment parentComment, LocalDateTime orderThan, Pageable pageable);
 }
