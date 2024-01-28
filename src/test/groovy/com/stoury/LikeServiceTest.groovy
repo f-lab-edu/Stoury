33a2fc85bc1aae76b79fe4c188a73509951fd239
Jzakka <mouse4786@gmail.com>
2024-01-28 16:34:57 +0900

    Test: 좋아요 서비스 캐싱로직 테스트 추가

--- src/test/groovy/com/stoury/LikeServiceTest.groovy
+++ src/test/groovy/com/stoury/LikeServiceTest.groovy
@@ -34,6 +34,7 @@ class LikeServiceTest extends Specification {
 
         then:
         1 * likeRedisRepository.save(_ as Like)
+        0 * likeDbRepository.save(_)
     }
 
     def "좋아요 실패 - 존재하지 않는 사용자"() {
@@ -54,7 +55,7 @@ class LikeServiceTest extends Specification {
         thrown(FeedSearchException.class)
     }
 
-    def "좋아요 실패 - 이미 좋아요 한 피드"() {
+    def "좋아요 실패 - 이미 좋아요 한 피드 - db에서 발견"() {
         setup:
         likeDbRepository.existsByMemberAndFeed(_ as Member, _ as Feed) >> true
 
@@ -65,18 +66,41 @@ class LikeServiceTest extends Specification {
         thrown(AlreadyLikedFeedException.class)
     }
 
-    def "좋아요 취소 성공"() {
+    def "좋아요 실패 - 이미 좋아요 한 피드 - 캐시에서 발견"() {
+        setup:
+        likeRedisRepository.existsByMemberAndFeed(_ as Member, _ as Feed) >> true
+
+        when:
+        likeService.like(1L, 2L);
+
+        then:
+        thrown(AlreadyLikedFeedException.class)
+    }
+
+    def "좋아요 취소 성공 - 캐시에서 삭제"() {
+        when:
+        likeService.likeCancel(1L, 2L)
+        then:
+        1 * likeRedisRepository.deleteByMemberAndFeed(_, _) >> true
+        0 * likeDbRepository.deleteByMemberAndFeed(_, _)
+    }
+
+    def "좋아요 취소 성공 - DB에서 삭제"() {
         when:
         likeService.likeCancel(1L, 2L)
 
         then:
-        1 * likeDbRepository.deleteByMemberAndFeed(liker, feed)
+        1 * likeRedisRepository.deleteByMemberAndFeed(_, _)
+        1 * likeDbRepository.deleteByMemberAndFeed(_, _)
     }
 
     def "특정 피드의 좋아요만 가져오기"() {
+        given:
+        likeRedisRepository.countByFeed(_ as Feed) >> 2
+        likeDbRepository.countByFeed(_ as Feed) >> 3
         when:
-        likeService.getLikesOfFeed(1L)
+        def likes = likeService.getLikesOfFeed(1L)
         then:
-        1 * likeDbRepository.countByFeed(_ as Feed)
+        likes == 5
     }
 }
