e68ad18bedcb5400c54919eb0c00104bf03560b8
Jzakka <mouse4786@gmail.com>
2024-01-28 12:14:03 +0900

    Fix: 댓글서비스 메서드가 객체 아이디를 받도록 수정

--- src/main/java/com/stoury/exception/CommentSearchException.java
+++ src/main/java/com/stoury/exception/CommentSearchException.java
@@ -1,6 +1,10 @@
 package com.stoury.exception;
 
 public class CommentSearchException extends RuntimeException {
+    public CommentSearchException() {
+        this("Cannot find comment");
+    }
+
     public CommentSearchException(String message) {
         super(message);
     }
--- src/main/java/com/stoury/exception/member/MemberSearchException.java
+++ src/main/java/com/stoury/exception/member/MemberSearchException.java
@@ -2,6 +2,7 @@ package com.stoury.exception.member;
 
 public class MemberSearchException extends RuntimeException {
     public MemberSearchException() {
+        this("Cannot find Member");
     }
 
     public MemberSearchException(String message) {
