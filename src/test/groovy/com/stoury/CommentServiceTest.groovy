cc46f84e60df60750d8c8519ea3a427f0ee512ac
Jzakka <mouse4786@gmail.com>
2024-01-28 12:14:03 +0900

    Fix: 댓글서비스 메서드가 객체 아이디를 받도록 수정

--- src/test/groovy/com/stoury/CommentServiceTest.groovy
+++ src/test/groovy/com/stoury/CommentServiceTest.groovy
@@ -7,61 +7,71 @@ import com.stoury.dto.CommentResponse
 import com.stoury.exception.CommentCreateException
 import com.stoury.exception.CommentSearchException
 import com.stoury.repository.CommentRepository
+import com.stoury.repository.FeedRepository
+import com.stoury.repository.MemberRepository
 import com.stoury.service.CommentService
-import com.stoury.validator.Validator
 import spock.lang.Specification
 
 import java.time.LocalDateTime
 
 class CommentServiceTest extends Specification {
-    def validator = Mock(Validator)
+    def memberRepository = Mock(MemberRepository)
+    def feedRepository = Mock(FeedRepository)
     def commentRepository = Mock(CommentRepository)
-    def commentService = new CommentService(commentRepository, validator)
+    def commentService = new CommentService(commentRepository, memberRepository, feedRepository)
 
     def member = Mock(Member)
     def feed = Mock(Feed)
     def savedComment = new Comment(member, feed, "This is comment")
-    def savedNestedComment = new Comment(member, savedComment, "This is nested comment")
+    def childComment = new Comment(member, savedComment, "This is child comment")
+
+    def setup() {
+        memberRepository.findById(_ as Long) >> Optional.of(member)
+        feedRepository.findById(_ as Long) >> Optional.of(feed)
+        commentRepository.findById(_ as Long) >> Optional.of(savedComment)
+    }
 
     def "댓글 생성 성공"() {
         when:
-        commentService.createComment(member, feed, "This is comment")
+        commentService.createComment(1L, 2L, "This is comment")
         then:
         1 * commentRepository.save(_ as Comment) >> savedComment
     }
 
     def "대댓글 생성 성공"() {
         when:
-        commentService.createNestedComment(member, savedComment, "This is nested comment")
+        commentService.createNestedComment(1L, 2L, "This is nested comment")
         then:
-        1 * commentRepository.save(_ as Comment) >> savedNestedComment
+        1 * commentRepository.save(_ as Comment) >> childComment
     }
 
     def "대대댓글 생성 실패"() {
         when:
-        commentService.createNestedComment(member, savedNestedComment, "This is double nested comment")
+        commentService.createNestedComment(1L, 2L, "This is double nested comment")
         then:
+        commentRepository.findById(_ as Long) >> Optional.of(childComment)
         thrown(CommentCreateException.class)
     }
 
     def "댓글 조회"() {
         when:
-        commentService.getCommentsOfFeed(feed, LocalDateTime.now())
+        commentService.getCommentsOfFeed(1L, LocalDateTime.now())
         then:
         1 * commentRepository.findAllByFeedAndCreatedAtBefore(_, _, _) >> List.of(savedComment)
     }
 
     def "대댓글 조회"() {
         when:
-        commentService.getNestedComments(savedComment, LocalDateTime.now())
+        commentService.getChildComments(1L, LocalDateTime.now())
         then:
-        1 * commentRepository.findAllByParentCommentAndCreatedAtBefore(_, _, _) >> List.of(savedNestedComment)
+        1 * commentRepository.findAllByParentCommentAndCreatedAtBefore(_, _, _) >> List.of(childComment)
     }
 
     def "대댓글 조회 실패 - 대댓글의 대댓글을 조회하려 함"() {
         when:
-        commentService.getNestedComments(savedNestedComment, LocalDateTime.now())
+        commentService.getChildComments(1L, LocalDateTime.now())
         then:
+        commentRepository.findById(1L) >> Optional.of(childComment)
         thrown(CommentSearchException.class)
     }
 
@@ -69,24 +79,10 @@ class CommentServiceTest extends Specification {
         given:
         def comment = new Comment(member, feed, "blabla")
         when:
-        commentService.deleteComment(comment)
+        commentService.deleteComment(1L)
         then:
+        commentRepository.findById(1L) >> Optional.of(comment)
         comment.isDeleted()
-    }
-
-    def "삭제된 댓글 긁어오면 '삭제된 댓글입니다'라고 표시"() {
-        given:
-        def comment1 = new Comment(member, feed, "comment1")
-        def comment2 = new Comment(member, feed, "comment2")
-        def comment3 = new Comment(member, savedComment, "comment3")
-
-        when:
-        commentService.deleteComment(comment2)
-        commentService.deleteComment(comment3)
-
-        then:
-        CommentResponse.from(comment1).textContent() == "comment1"
-        CommentResponse.from(comment2).textContent() == "This comment was deleted"
-        CommentResponse.from(comment3).textContent() == "This comment was deleted"
+        CommentResponse.from(comment).textContent() == "This comment was deleted"
     }
 }
