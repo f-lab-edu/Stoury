1942422edc7a5483be7a04fc313d3b1392303b92
Jzakka <mouse4786@gmail.com>
2024-01-30 11:43:30 +0900

    Refactor: 유효성 검증을 메서드 추출

--- src/main/java/com/stoury/repository/LikeRedisRepository.java
+++ src/main/java/com/stoury/repository/LikeRedisRepository.java
@@ -21,30 +21,45 @@ public class LikeRedisRepository {
     }
 
     public Like save(Like like) {
-        String feedId = Objects.requireNonNull(like.getFeed().getId(), "Feed id cannot be null").toString();
-        String memberId = Objects.requireNonNull(like.getMember().getId(), "Member id cannot be null").toString();
+        String feedId = getFeedIdToString(like.getFeed());
+        String memberId = getMemberIdToString(like.getMember());
         opsForSet.add(feedId, memberId);
         return like;
     }
 
     public boolean existsByMemberAndFeed(Member liker, Feed feed) {
-        String memberId = Objects.requireNonNull(liker.getId(), "Member id cannot be null").toString();
-        String feedId = Objects.requireNonNull(feed.getId(), "Feed id cannot be null").toString();
+        String memberId = getMemberIdToString(liker);
+        String feedId = getFeedIdToString(feed);
 
         return opsForSet.isMember(feedId, memberId);
     }
 
     public boolean deleteByMemberAndFeed(Member liker, Feed feed) {
         if (existsByMemberAndFeed(liker, feed)) {
-            opsForSet.remove(feed.getId().toString(), liker.getId().toString());
+            String memberId = getMemberIdToString(liker);
+            String feedId = getFeedIdToString(feed);
+
+            opsForSet.remove(feedId, memberId);
             return true;
         }
         return false;
     }
 
     public int countByFeed(Feed feed) {
-        String feedId = Objects.requireNonNull(feed.getId(), "Feed id cannot be null").toString();
+        String feedId = getFeedIdToString(feed);
         return Math.toIntExact(opsForSet.size(feedId));
+    }
+
+    private String getFeedIdToString(Feed feed) {
+        Long feedId = Objects.requireNonNull(feed.getId(), "Feed id cannot be null");
+
+        return feedId.toString();
+    }
+
+    private String getMemberIdToString(Member member) {
+        Long memberId = Objects.requireNonNull(member.getId(), "Member id cannot be null");
+
+        return memberId.toString();
     }
 
     private void clearAllOnlyForTest() {
