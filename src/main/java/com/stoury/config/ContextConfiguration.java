f255c008e86e4ace5c5040d72fd75d248cf925e9
Jzakka <mouse4786@gmail.com>
2024-02-03 13:34:59 +0900

    Fix: 오타수정

--- src/main/java/com/stoury/config/ContextConfiguration.java
+++ src/main/java/com/stoury/config/ContextConfiguration.java
@@ -1,6 +1,5 @@
 package com.stoury.config;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.google.maps.GeoApiContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +17,7 @@ public class ContextConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @JsonAnyGetter
+    @Bean
     public GeoApiContext geoApiContext() {
         return new GeoApiContext.Builder()
                 .apiKey(apiKey)
