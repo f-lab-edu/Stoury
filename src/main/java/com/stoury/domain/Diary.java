f3b3cdf9e756656a284235dbae62daf3aa4874cc
Jzakka <mouse4786@gmail.com>
2024-02-06 17:22:55 +0900

    Feat: 여행일지 작성

---
+++ src/main/java/com/stoury/domain/Diary.java
@@ -0,0 +1,35 @@
+package com.stoury.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "DIARY")
+public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Member member;
+
+    @JoinTable(joinColumns = @JoinColumn(name = "DIARY_ID"))
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = false)
+    private List<Feed> feeds = new ArrayList<>();
+
+    @Column(name = "TITLE", length = 50)
+    private String title;
+
+    public Diary(Member member, List<Feed> feeds, String title) {
+        this.member = member;
+        this.feeds = feeds;
+        this.title = title;
+    }
+}
