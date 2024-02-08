1ec063fdf2364b44de3a9e63cb53658428427123
Jzakka <mouse4786@gmail.com>
2024-02-08 15:47:37 +0900

    Feat: 여행일지 취소
    - Add:여행일지 생성 예외 클래스
    - Add:여행일지 조회 예외 클래스

--- src/main/java/com/stoury/dto/diary/SimpleDiaryResponse.java
+++ src/main/java/com/stoury/dto/diary/SimpleDiaryResponse.java
@@ -2,8 +2,8 @@ package com.stoury.dto.diary;
 
 import com.stoury.domain.Diary;
 
-public record SimpleDiaryResponse(Long id, String thumbnail, String title) {
+public record SimpleDiaryResponse(Long id, String thumbnail, String title, Long memberId) {
     public static SimpleDiaryResponse from(Diary diary) {
-        return new SimpleDiaryResponse(diary.getId(), diary.getThumbnailPath(), diary.getTitle());
+        return new SimpleDiaryResponse(diary.getId(), diary.getThumbnailPath(), diary.getTitle(), diary.getMember().getId());
     }
 }
---
+++ src/main/java/com/stoury/exception/diary/DiarySearchException.java
@@ -0,0 +1,4 @@
+package com.stoury.exception.diary;
+
+public class DiarySearchException extends RuntimeException {
+}
