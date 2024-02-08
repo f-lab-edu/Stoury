97e97be9e1d745e5c7c31b1b90ade6762b2a31bb
Jzakka <mouse4786@gmail.com>
2024-02-08 15:57:09 +0900

    Feat: 컨트롤러에 diaryService 연결

--- src/main/java/com/stoury/controller/DiaryController.java
+++ src/main/java/com/stoury/controller/DiaryController.java
@@ -2,13 +2,14 @@ package com.stoury.controller;
 
 import com.stoury.dto.diary.DiaryCreateRequest;
 import com.stoury.dto.diary.DiaryResponse;
+import com.stoury.dto.diary.SimpleDiaryResponse;
 import com.stoury.dto.member.AuthenticatedMember;
 import com.stoury.service.DiaryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +20,17 @@ public class DiaryController {
     public DiaryResponse createDiary(@AuthenticationPrincipal AuthenticatedMember authenticatedMember,
                                      @RequestBody(required = true) DiaryCreateRequest diaryCreateRequest) {
         return diaryService.createDiary(diaryCreateRequest, authenticatedMember.getId());
+    }
+
+    @GetMapping("/diaries/member/{memberId}")
+    public Page<SimpleDiaryResponse> getMemberDiaries(@PathVariable Long memberId,
+                                                      @RequestParam(required = false, defaultValue = "0") int pageNo) {
+        return diaryService.getMemberDiaries(memberId, pageNo);
+    }
+
+    @DeleteMapping("/diaries/{diaryId}")
+    public ResponseEntity<Object> cancelDiary(@PathVariable Long diaryId) {
+        diaryService.cancelDiary(diaryId);
+        return ResponseEntity.ok().build();
     }
 }
