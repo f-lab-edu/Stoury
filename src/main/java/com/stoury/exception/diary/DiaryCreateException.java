2047a9c9cae2b23994e6d7e1919c2f321fa58f55
Jzakka <mouse4786@gmail.com>
2024-02-08 15:47:37 +0900

    Feat: 여행일지 취소
    - Add:여행일지 생성 예외 클래스
    - Add:여행일지 조회 예외 클래스

---
+++ src/main/java/com/stoury/exception/diary/DiaryCreateException.java
@@ -0,0 +1,7 @@
+package com.stoury.exception.diary;
+
+public class DiaryCreateException extends RuntimeException {
+    public DiaryCreateException(String message) {
+        super(message);
+    }
+}
