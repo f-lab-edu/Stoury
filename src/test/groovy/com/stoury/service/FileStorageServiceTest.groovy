bb731dfbbc08f05521bf82684bb81ac0e6cff35a
Jzakka <mouse4786@gmail.com>
2024-02-23 14:07:45 +0900

    Fix: 파일의 부모 경로가 없으면 생성하도록 수정

---
+++ src/test/groovy/com/stoury/service/FileStorageServiceTest.groovy
@@ -0,0 +1,31 @@
+package com.stoury.service
+
+import com.stoury.service.storage.FileStorageService
+import com.stoury.utils.FileUtils
+import org.springframework.mock.web.MockMultipartFile
+import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class FileStorageServiceTest extends Specification {
+    def fileStorageService = new FileStorageService()
+    final String PATH_PREFIX = "src/test/resources/storagetest";
+    final Path startPath = Paths.get(PATH_PREFIX);
+
+
+    def cleanup() {
+        FileUtils.deleteRecursivelyDirectory(startPath);
+    }
+
+    def "지정경로에 파일 생성"() {
+        given:
+        MockMultipartFile file = new MockMultipartFile("Something", "file1", "image/jpeg", new byte[0]);
+        def path = Paths.get(PATH_PREFIX + "/file1.jpeg")
+        when:
+        fileStorageService.saveFileAtPath(file, path)
+        then:
+        Files.exists(path)
+    }
+}
