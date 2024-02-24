c6e7f27f8be79e18f91e2f1d23588c74a21063cd
Jzakka <mouse4786@gmail.com>
2024-02-24 20:33:49 +0900

    Feat: 헤더 text/html으로 수정

--- src/main/java/com/stoury/controller/HomeController.java
+++ src/main/java/com/stoury/controller/HomeController.java
@@ -2,12 +2,15 @@ package com.stoury.controller;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public Resource lobby() {
         return new ClassPathResource("static/docs/index.html");
     }
