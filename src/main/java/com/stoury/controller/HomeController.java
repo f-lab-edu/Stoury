3ff72e7e7a98fd13dac3762761d434e455b1df18
Jzakka <mouse4786@gmail.com>
2024-02-24 20:40:31 +0900

    Feat: CrossOrigin 삭제

--- src/main/java/com/stoury/controller/HomeController.java
+++ src/main/java/com/stoury/controller/HomeController.java
@@ -3,11 +3,9 @@ package com.stoury.controller;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class HomeController {
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
