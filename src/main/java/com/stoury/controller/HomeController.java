fa70015e147528ad0c2f1f4203b75ae5ce7a2935
Jzakka <mouse4786@gmail.com>
2024-02-25 14:05:25 +0900

    Feat: 홈 컨트롤러 경로 모든 메서드 다 뚫음

--- src/main/java/com/stoury/controller/HomeController.java
+++ src/main/java/com/stoury/controller/HomeController.java
@@ -3,12 +3,12 @@ package com.stoury.controller;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public Resource lobby() {
         return new ClassPathResource("static/docs/index.html");
     }
