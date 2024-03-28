30ea99e71d902e51b53c43792c47679bec3d2833
Jzakka <mouse4786@gmail.com>
2024-03-28 14:52:48 +0900

    Fix: 이벤트 핸들러가 비동기로 동작하도록 수정

--- src/main/java/com/stoury/StouryApplication.java
+++ src/main/java/com/stoury/StouryApplication.java
@@ -3,9 +3,11 @@ package com.stoury;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class StouryApplication {
 
     public static void main(String[] args) {
